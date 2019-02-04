0ad78a400f8d7347d34de4c10cd25dfa9127a118
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-02-04 19:18:50 +0100

    minor tweaks; bells and whistles

--- WebCrawler.py
+++ WebCrawler.py
@@ -9,12 +9,18 @@ def site_map(url):
 
     def create_map(current_url):
         site = requests.get(current_url).text
-        soup = BeautifulSoup(site, 'lxml')
-        title = soup.find('title').text
+        soup = BeautifulSoup(site, 'html.parser')
+        title = soup.find('title').text if soup.find('title') else None
         links = soup.find_all('a')
-        links = set(link['href'] for link in links if link['href'].startswith(domain) or link['href'].startswith('/'))
+        #  extract href values from <a>'s, discard ones which href's value is empty and discard duplicates
+        links = set(link.get('href') for link in links if link.get('href'))
+        #  screen out hrefs that do not lead to current domian's pages
+        links = set(link for link in links if link.startswith(domain) or link.startswith('/'))
+        #  prepend current domain to links that do not have it specified explicitly
         links = set(link if link.startswith(domain) else f'{domain}{link}' for link in links)
+        links = set(link[:-1] if link.endswith('/') else link for link in links)
         result[current_url] = {'title': title, 'links': links}
+        print(f'Links added: {len(result)}', end='\r')
         for link in links:
             if result.get(link):  # if key of given link already exist in result dict
                 pass
